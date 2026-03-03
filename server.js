b6608714f3e81e146aae61404cec05fbcfbe93a8
Jason Cheng <164652284+JasonCheng31@users.noreply.github.com>
2026-03-03 13:37:02 -0500

    change 2

--- server.js
+++ server.js
@@ -41,4 +41,6 @@ app.get("/test-stripe", async (req, res) => {
     res.status(500).send(err.message);
   }
 });
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
